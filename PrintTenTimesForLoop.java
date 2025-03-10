1768a38b6a8adb8b36ac9b2fe8b361cffc9b2dd8
swathisripathi15 <125676002+swathisripathi15@users.noreply.github.com>
2025-03-10 13:26:53 +0530

    Add files via upload

---
+++ PrintTenTimesForLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class PrintTenTimesForLoop {
+    public static void main(String[] args) {
+        //If the number of iteration is fixed then use for loop
+        // Syntax : for(initialization ; condition ; increment/decrement)
+        //loop Executes from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ".Bright IT Career");
+        }
+    }
+}
