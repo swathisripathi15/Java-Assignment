dd42e4b4f3541936f30fc552ebd2a04893ceb49c
swathisripathi15 <125676002+swathisripathi15@users.noreply.github.com>
2025-03-10 13:21:30 +0530

    Add files via upload

---
+++ CreateOwnException.java
@@ -0,0 +1,33 @@
+package Exceptions;
+
+class InvalidExamException extends Exception {
+    InvalidExamException(String msg) {
+        System.out.println(msg);
+    }
+}
+
+//creating our own exception
+public class CreateOwnException {
+
+    //creating a method which receives integer as parameter
+    static void exam(int marks) throws InvalidExamException {
+        // throws exception if marks are less than 40
+        if (marks < 40) {
+            //InvalidExamException is our own custom exception
+            throw new InvalidExamException("Failed in exam");
+        } else {
+            System.out.println("Passed in exam");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            //passing marks to the method
+            exam(39);
+        } catch (Exception i) {
+            /* printStackTrace() prints the throwable Exception object as well as with other Information
+            like the line number where Exception occurs and class name where the exception occurred.*/
+            i.printStackTrace();
+        }
+    }
+}
---
+++ GenerateIOException.java
@@ -0,0 +1,20 @@
+package Exceptions;
+
+public class GenerateIOException {
+    public static void main(String[] args) {
+
+        File file = new File("test.txt");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            fis.read();
+            fis.close();
+        }
+        //FileNotFoundException is a subclass of IOException.
+        catch (IOException e) {
+            /* printStackTrace() prints the throwable Exception object as well as with other Information
+            like the line number where Exception occurs and class name where the exception occurred.*/
+            e.printStackTrace();
+        }
+    }
+}
