00f47668a665e94643d9cd557431367a99b34c76
swathisripathi15 <125676002+swathisripathi15@users.noreply.github.com>
2025-03-10 13:28:23 +0530

    Add files via upload

---
+++ Equal_NotEqual.java
@@ -0,0 +1,25 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class Equal_NotEqual {
+
+    public static void main(String[] args) {
+        /* To get the instance of Java Scanner which reads input from the user,
+         we need to pass the input stream (System.in) in the constructor of Scanner class. */
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        // "nextInt()" scans the next token of the input as an Int.
+        int a = num.nextInt();
+        System.out.print("Enter second number : ");
+        int b = num.nextInt();
+        //equal operator (==)
+        if (a == b) {
+            System.out.println("Both numbers are equal");
+        }
+        //not equal operator (!=)
+        else if (a != b) {
+            System.out.println("Both  numbers are different");
+        }
+    }
+}
